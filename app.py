fd5dce3f549203dc85ad3f794fb1397428cae684
infinith4 <infinith4@gmail.com>
2018-07-07 22:51:02 +0900

    add image

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # coding: utf-8
-import os
+from os.path import join, relpath
+from glob import glob
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,8 +11,19 @@ def hello():
 
 @app.route("/")
 def index():
+    path = "./static"
+    image_names = [relpath(x, path) for x in glob(join(path, '*.jpeg'))]
+    # static内の.pngファイルがimage_namesに格納されます。
+    my_list = []
+    for image in image_names:
+        my_dic = {}
+        my_dic['image_name'] = 'static/' + image
+        my_dic['link_url'] = 'https://github.com/infinith4'
+        # 飛び先のURLを何個も確保できなかったのでどの画像に対しても同じURLに飛ぶようにしました。
+        my_list.append(my_dic)
+
     message = 'サンプル メッセージ'
-    return render_template('index.html', message=message)
+    return render_template('index.html', message=my_list)
 
 if __name__ == '__main__':
     app.run()
